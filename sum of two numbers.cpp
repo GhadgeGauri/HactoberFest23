c8f261a86e5e736b08ced6186ae08d1240113e9e
AMBATI KRISHNA TEJA <kittoteja@gmail.com>
2023-10-04 06:16:34 +0530

    Create sum of two numbers.cpp

---
+++ sum of two numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    // Declare two variables to store the numbers
+    double num1, num2;
+
+    // Prompt the user to enter the first number
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+
+    // Prompt the user to enter the second number
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+    // Calculate the sum
+    double sum = num1 + num2;
+
+    // Display the result
+    std::cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << std::endl;
+
+    return 0;
+}
