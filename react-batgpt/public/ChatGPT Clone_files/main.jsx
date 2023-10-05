7338a7b4a391461c6556d56b3bc67d61eb73e029
Saie12 <saieshsasane@gmail.com>
2023-10-05 23:49:17 +0530

    🤞Added react-batgpt folder🎉

---
+++ react-batgpt/public/ChatGPT Clone_files/main.jsx
@@ -0,0 +1,16 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=4dfba374"; const _jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=14ac61d4"; const React = __vite__cjsImport1_react.__esModule ? __vite__cjsImport1_react.default : __vite__cjsImport1_react;
+import __vite__cjsImport2_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=513c2f66"; const ReactDOM = __vite__cjsImport2_reactDom_client.__esModule ? __vite__cjsImport2_reactDom_client.default : __vite__cjsImport2_reactDom_client;
+import App from "/src/App.jsx?t=1683577350529";
+import "/src/index.css?t=1683577407041";
+ReactDOM.createRoot(document.getElementById('root')).render(/*#__PURE__*/ _jsxDEV(React.StrictMode, {
+    children: /*#__PURE__*/ _jsxDEV(App, {}, void 0, false, {
+        fileName: "/Users/kas1qqqq/Desktop/Projects/react-chatgpt-clone/src/main.jsx",
+        lineNumber: 8,
+        columnNumber: 5
+    }, this)
+}, void 0, false, {
+    fileName: "/Users/kas1qqqq/Desktop/Projects/react-chatgpt-clone/src/main.jsx",
+    lineNumber: 7,
+    columnNumber: 3
+}, this));
